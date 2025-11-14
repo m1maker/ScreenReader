88e1bba22ad4ba6499e680d31cb1883b2bc82eef
m1maker <georgijbondarenko248@gmail.com>
2025-11-14 19:18:19 +0300

    Log standard exceptions.

--- SRC/main.cpp
+++ SRC/main.cpp
@@ -1,8 +1,20 @@
 // Program entry point.
 #include "App.h"
+#include <exception>
 
 signed int main(signed int argc, char** argv) {
-	g_applicationInstance; // Call the application constructor.
+	/*
+	We log exceptions directly from main, not from CScreenReaderApp.
+
+	We'll simply log anything we don't handle in other parts of the application.
+	*/
+	try {
+		g_applicationInstance; // Call the application constructor.
+	}
+	catch (std::exception& standard_exception) {
+		g_logger.Log(CLogger::ERROR, "Application", standard_exception.what());
+	}
+
 	return g_retcode;
 }
 
