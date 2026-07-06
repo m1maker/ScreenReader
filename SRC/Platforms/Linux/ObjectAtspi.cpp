4bed58575dff1f85f4546d1da62aa2b87f9452de
m1maker <georgijbondarenko248@gmail.com>
2026-07-06 21:21:56 +0300

    Platforms.Linux.Object: Complete fetching the main provider.

--- SRC/Platforms/Linux/ObjectAtspi.cpp
+++ SRC/Platforms/Linux/ObjectAtspi.cpp
@@ -38,4 +38,59 @@ void ObjectAtspiFetch(const SObjectFetchRequest*& request) noexcept {
 		auto native_role = atspi_accessible_get_role(native_handle, nullptr);
 		slot->type = GetObjectTypeFromAtspiRole(native_role);
 	}
+	if (request->mask.test(std::to_underlying(EObjectFetchValue::STATES))) {
+		auto native_states = atspi_accessible_get_state_set(native_handle);
+		slot->states = GetMergedObjectStateFromAtspiStates(native_states).states;
+		g_object_unref(native_states);
+	}
+	if (request->mask.test(std::to_underlying(EObjectFetchValue::CAPABILITIES))) {
+		auto native_states = atspi_accessible_get_state_set(native_handle);
+		slot->capabilities = GetMergedObjectStateFromAtspiStates(native_states).capabilities;
+		g_object_unref(native_states);
+	}
+	if (request->mask.test(std::to_underlying(EObjectFetchValue::PARENT))) {
+		auto native_parent = atspi_accessible_get_parent(native_handle, nullptr);
+		if (!native_parent) {
+			slot->parent = std::unexpected(EObjectError::FAIL);
+		}
+		else
+			slot->parent = TObjectCache<AtspiAccessible*>::GetInstance().GetOrCreate(native_handle);
+	}
+	if (request->mask.test(std::to_underlying(EObjectFetchValue::CHILDREN))) {
+		auto native_children_count = atspi_accessible_get_child_count(native_handle, nullptr);
+		slot->children->resize(native_children_count);
+		for (auto i = 0; i < native_children_count; ++i) {
+			auto native_child = atspi_accessible_get_child_at_index(native_handle, i, nullptr);
+			if (!native_child)
+				continue;
+			slot->children->operator[](i) = TObjectCache<AtspiAccessible*>::GetInstance().GetOrCreate(native_child);
+		}
+	}
+	if (request->mask.test(std::to_underlying(EObjectFetchValue::TOOLKIT_NAME))) {
+		auto toolkit_name = atspi_accessible_get_toolkit_name(native_handle, nullptr);
+		if (!toolkit_name) {
+			slot->toolkit_name = std::unexpected(EObjectError::FAIL);
+		}
+		else
+			slot->toolkit_name = toolkit_name;
+		g_free(toolkit_name);
+	}
+	if (request->mask.test(std::to_underlying(EObjectFetchValue::NAME))) {
+		auto name = atspi_accessible_get_name(native_handle, nullptr);
+		if (!name) {
+			slot->name = std::unexpected(EObjectError::FAIL);
+		}
+		else
+			slot->name = name;
+		g_free(name);
+	}
+	if (request->mask.test(std::to_underlying(EObjectFetchValue::DESCRIPTION))) {
+		auto description = atspi_accessible_get_description(native_handle, nullptr);
+		if (!description) {
+			slot->name = std::unexpected(EObjectError::FAIL);
+		}
+		else
+			slot->description = description;
+		g_free(description);
+	}
 }
